4334c5b70544645a71580ad15bcba43c85f2b0f1
Alejandro Montilla <amontilla@luckycycle.com>
2022-10-21 21:12:58 +0200

    Change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './App.css';
 import { Button, Colors } from './components/Button';
 import { Title } from './components/Title';
 import './styles/styles.css'
